847fd9eac55b6f1849ee80f6d272b025efe9224f
tricia gray <tgray@tricias-MacBook-Pro.local>
2023-04-11 19:04:54 -0400

    initial heroku commit

---
+++ database.sql
@@ -0,0 +1,14 @@
+-- CREATE DATABASE cms;
+
+CREATE TABLE cms(
+    id SERIAL PRIMARY KEY,
+    date varchar(255), 
+    movie varchar(255),
+    short varchar(255),
+    tv_series varchar(255),
+    book varchar(255),
+    play varchar(255),
+    short_story varchar(255)
+);
+
+INSERT INTO cms (date, movie, short) VALUES ($1,$2, $3);
---
+++ postgres.js
@@ -0,0 +1,26 @@
+const Client = require('pg').Client
+// require("dotenv").config();
+
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const proConfig = process.env.DATABASE_URL; 
+// heroku addons
+
+// const dbConfig = {
+// 	connectionString:  process.env.NODE_ENV === "production" ? proConfig : devConfig,
+// }
+
+const dbConfig = {
+	connectionString: 'postgresql://localhost:5432/fragments-server',
+}
+
+if (process.env.DATABASE_URL){
+	dbConfig.ssl = { rejectUnauthorized: false }
+	dbConfig.connectionString = process.env.DATABASE_URL
+
+}
+
+const client = new Client(dbConfig)
+
+
+module.exports = client;
