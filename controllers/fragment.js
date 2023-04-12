bfb1505f7429fa9ce80433cc98a7059ee18d7d09
tricia gray <tgray@tricias-MacBook-Pro.local>
2023-04-12 15:26:28 -0400

    revised code

--- controllers/fragment.js
+++ controllers/fragment.js
@@ -1,35 +1,50 @@
-// const express = require('express');
-// const router = express.Router();
-// const postgres = require('../postgres.js');
+const express = require('express');
+const router = express.Router();
+const postgres = require('../postgres.js');
 
-// router.get('/', (req, res) => {
-//     postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
-//         res.json(results.rows)
-//     });
-// });
 
-// router.post('/', (req, res) => {
-//     postgres.query(`INSERT INTO cms (date, movie, short, tv_series, book, play, short_story) VALUES ('${req.body.date}', ${req.body.movie}, ${req.body.short}, ${req.body.tv_series}, ${req.body.book}, ${req.body.play}, ${req.body.short_story})`, (err, results) => {
-//         postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
-//             res.json(results.rows)
-//         });
-//     })
-// });
+router.get('/', (req, res) => {
+    postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
+        res.json(results.rows)
+    });
+});
 
-// router.delete('/:id', (req, res) => {
-//     postgres.query(`DELETE FROM cms WHERE id = ${req.params.id};`, (err, results) => {
-//         postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
-//             res.json(results.rows)
-//         });
-//     });
-// });
+router.get('/:id', (req, res) => {
+    postgres.query(`SELECT * FROM cms WHERE id = ${req.params.id};`, (err, results) => {
+        res.json(results.rows)
+    });
+});
 
-// router.put('/:id', (req, res) => {
-//     postgres.query(`UPDATE cms SET date = '${req.body.date}', movie = ${req.body.movie}, short = ${req.body.short}, tv_series = ${req.body.tv_series}, book = ${req.body.book}, play = ${req.body.play}, short_story = ${req.body.short_story} WHERE id = ${req.params.id}`, (err, results) => {
-//         postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
-//             res.json(results.rows)
-//         });
-//     })
-// });
+router.post('/', (req, res) => {
+    postgres.query(`INSERT INTO cms (date, movie, short, tv_series, book, play, short_story) VALUES ('${req.body.date}', '${req.body.movie}', '${req.body.short}', '${req.body.tv_series}', '${req.body.book}', '${req.body.play}', '${req.body.short_story}')`, (err, results) => {
+        postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
+            if (err) {
+                console.log(err);
+            } else {
+            console.log(req.body);
+            res.json(results.rows)
+            }
+        });
+    })
+});
 
-// module.exports = router;
+router.delete('/:id', (req, res) => {
+    postgres.query(`DELETE FROM cms WHERE id = ${req.params.id};`, (err, results) => {
+        postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
+            res.json(results.rows)
+        });
+    });
+});
+
+router.put('/:id', (req, res) => {
+    postgres.query(`UPDATE cms SET date = '${req.body.date}', movie = '${req.body.movie}', short = '${req.body.short}', tv_series = '${req.body.tv_series}', book = '${req.body.book}', play = '${req.body.play}', short_story = '${req.body.short_story}' WHERE id = ${req.params.id}`, (err, results) => {
+        postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
+            res.json(results.rows)
+        });
+    })
+});
+
+
+
+
+module.exports = router
--- postgres.js
+++ postgres.js
@@ -1,20 +1,37 @@
-const Client = require('pg').Client
-// require("dotenv").config();
+// const Client = require('pg').Client
+// // require("dotenv").config();
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL; 
-// heroku addons
+// const proConfig = process.env.DATABASE_URL; 
+// // heroku addons
+
+// // const dbConfig = {
+// // 	connectionString:  process.env.NODE_ENV === "production" ? proConfig : devConfig,
+// // }
 
 // const dbConfig = {
-// 	connectionString:  process.env.NODE_ENV === "production" ? proConfig : devConfig,
+// 	connectionString: 'postgresql://localhost:5432/frag-ments',
 // }
+
+// if (process.env.DATABASE_URL){
+// 	dbConfig.ssl = { rejectUnauthorized: false }
+// 	dbConfig.connectionString = process.env.DATABASE_URL
+
+// }
+
+// const client = new Client(dbConfig)
+
+
+// module.exports = client;
+
+const Client = require('pg').Client
 
 const dbConfig = {
 	connectionString: 'postgresql://localhost:5432/frag-ments',
 }
 
-if (process.env.DATABASE_URL){
+if(process.env.DATABASE_URL){
 	dbConfig.ssl = { rejectUnauthorized: false }
 	dbConfig.connectionString = process.env.DATABASE_URL
 
