5212dbdc958ee4a660f41965c85436e6be775f13
tricia gray <tgray@tricias-MacBook-Pro.local>
2023-04-12 21:22:19 -0400

    updates code for routes

--- controllers/fragment.js
+++ controllers/fragment.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const postgres = require('../postgres.js');
 
 
-router.get('/', (req, res) => {
+router.get('/fragments', (req, res) => {
     postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
         res.json(results.rows)
     });
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 //     });
 // });
 
-router.post('/', (req, res) => {
+router.post('/fragments/', (req, res) => {
     postgres.query(`INSERT INTO cms (date, movie, short, tv_series, book, play, short_story) VALUES ('${req.body.date}', '${req.body.movie}', '${req.body.short}', '${req.body.tv_series}', '${req.body.book}', '${req.body.play}', '${req.body.short_story}')`, (err, results) => {
         postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
             if (err) {
@@ -28,7 +28,7 @@ router.post('/', (req, res) => {
     })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete(`/fragments/${id}`, (req, res) => {
     postgres.query(`DELETE FROM cms WHERE id = ${req.params.id};`, (err, results) => {
         postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
             res.json(results.rows)
@@ -36,7 +36,7 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
+router.put(`/fragments/${id}`, (req, res) => {
     postgres.query(`UPDATE cms SET date = '${req.body.date}', movie = '${req.body.movie}', short = '${req.body.short}', tv_series = '${req.body.tv_series}', book = '${req.body.book}', play = '${req.body.play}', short_story = '${req.body.short_story}' WHERE id = ${req.params.id}`, (err, results) => {
         postgres.query('SELECT * FROM cms ORDER BY id ASC;', (err, results) => {
             res.json(results.rows)
